07a08b2dca089fe5a8eddafcc9c0c359ab5c0bf7
freesky <topsword@163.com>
2018-04-25 17:39:06 +0800

    增加异步通讯Future模拟

---
+++ src/com/java/base/Future/Data.java
@@ -0,0 +1,8 @@
+package com.java.base.Future;
+
+/**
+ * Created by yw on 2018/4/25.
+ */
+public interface Data {
+    public String  getRequest();
+}
---
+++ src/com/java/base/Future/FutureData.java
@@ -0,0 +1,37 @@
+package com.java.base.Future;
+
+
+
+/**
+ * 包装类
+ * Created by yw on 2018/4/25.
+ */
+public class FutureData implements Data  {
+
+    private RealData realData;
+
+    private boolean isReady= false;
+
+
+    public synchronized void setRealData(RealData realData) {
+        if(isReady) {
+            return ;
+        }
+        this.realData = realData;
+        isReady = true;
+        notify();//唤醒获取结果线程
+    }
+
+    @Override
+    public synchronized String getRequest() {
+        while(!isReady){
+            try {
+                wait();//阻塞到此处
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        return  realData.getResult();
+    }
+
+}
