3732c1224adc791152054fa430612f876bf3d569
freesky <topsword@163.com>
2018-05-07 17:20:41 +0800

    NIO server& client

---
+++ src/com/java/base/socket/nio/NioClient.java
@@ -0,0 +1,73 @@
+package com.java.base.socket.nio;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+import java.util.Scanner;
+
+/**
+ * //                            _ooOoo_
+ * //                           o8888888o
+ * //                           88" . "88
+ * //                           (| -_- |)
+ * //                            O\ = /O
+ * //                        ____/`---'\____
+ * //                      .   ' \\| |// `.
+ * //                       / \\||| : |||// \
+ * //                     / _||||| -:- |||||- \
+ * //                       | | \\\ - /// | |
+ * //                     | \_| ''\---/'' | |
+ * //                      \ .-\__ `-` ___/-. /
+ * //                   ___`. .' /--.--\ `. . __
+ * //                ."" '< `.___\_<|>_/___.' >'"".
+ * //               | | : `- \`.;`\ _ /`;.`/ - ` : | |
+ * //                 \ \ `-. \_ __\ /__ _/ .-` / /
+ * //         ======`-.____`-.___\_____/___.-`____.-'======
+ * //                            `=---='
+ * //
+ * //         .............................................
+ * //                  佛祖保佑             永无BUG
+ * //          佛曰:
+ * //                  写字楼里写字间，写字间里程序员；
+ * //                  程序人员写程序，又拿程序换酒钱。
+ * //                  酒醒只在网上坐，酒醉还来网下眠；
+ * //                  酒醉酒醒日复日，网上网下年复年。
+ * //                  但愿老死电脑间，不愿鞠躬老板前；
+ * //                  奔驰宝马贵者趣，公交自行程序员。
+ * //                  别人笑我忒疯癫，我笑自己命太贱；
+ * //                  不见满街漂亮妹，哪个归得程序员？
+ * Created by yw on 2018/5/7.
+ */
+public class NioClient {
+
+    public static void main(String[] args) {
+
+        try {
+            SocketChannel socketChannel = SocketChannel.open();
+            socketChannel.configureBlocking(false);
+            socketChannel.connect(new InetSocketAddress("localhost",6666));
+
+            while(true){
+                System.out.println(   socketChannel.finishConnect());
+                BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+//                Scanner scanner = new Scanner(System.in);
+                String res = reader.readLine();
+                ByteBuffer buffer = ByteBuffer.allocate(1024);
+                buffer.put(res.getBytes());
+                buffer.flip();
+                while(buffer.hasRemaining()){
+                    socketChannel.write(buffer);
+                }
+                System.out.println("客户端发送："+res);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+}
